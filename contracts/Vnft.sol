c7bec7951df09cd0fc03e5265f3a011073c9d474
Othnapcalx <othnapcalx@kali>
2023-09-14 15:26:23 +0100

    Vnft initial commit

--- contracts/Vnft.sol
+++ contracts/Vnft.sol
@@ -1,6 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.19;
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -12,6 +11,7 @@ contract Cheytac is ERC721, VRFConsumerBase, Ownable {
     uint256 public randomResult;
     address public VRFCoordinator = "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D";
     address public LinkToken = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
+    uint64 private constant suscriptionId = 14223;
    
     struct Equipment {
         uint256 size;
